61da52eaaea75b13b6c55d99c2704e17a3eeb1d8
ahmed alawar <af47037@gmail.com>
2023-12-02 10:43:53 -0800

    last digit variable fixing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,5 @@ if l_digit > 5:
     print(F"Last digit of {number} is {l_digit} and is greater than 5")
 if l_digit == 0:
     print(F"Last digit of {number} is {l_digit} and is 0")
-if l_digit < 6 and last_digit != 0:
+if l_digit < 6 and l_digit != 0:
     print(F'Last digit of {number} is {l_digit} and is less than 6 and not 0')
