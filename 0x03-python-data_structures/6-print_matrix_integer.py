77fb6f6134b0e40aaa81f27c7f2668fe1ce4ebf9
ahmed alawar <af47037@gmail.com>
2023-12-04 18:11:03 -0800

    Fixing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         leng = 1
         for i in row:
-            if l == len(row):
+            if leng == len(row):
                 print("{:d}".format(i), end="")
             else:
                 print("{:d}".format(i), end=" ")
             leng += 1
+        print("")
+
