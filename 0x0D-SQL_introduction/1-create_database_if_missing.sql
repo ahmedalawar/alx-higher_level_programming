36ab9b5e13c4145fb3d12a6307816e71a53f4cac
ahmed alawar <af47037@gmail.com>
2024-02-13 18:03:31 -0800

    mysql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- This script check if hbtn_0c0_0 is already exits if not create it
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- check if Database exist and delete it.
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
